920bd9765c608a0412079700ed15eac78f8fb23d
Milkah Wangui Ng'ang'a <56192567+wangui-nganga@users.noreply.github.com>
2024-12-08 20:07:23 +0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,18 +1,24 @@
-from django.urls import path
+"""
+URL configuration for physiotherapy project.
 
-from . import views
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
 
-urlpatterns=[
-    path('', views.index, name='Home'),
-    path('blog/',views.blog,name='Blog'),
-    path('feature/',views.feature,name='Feature'),
-    path('about/',views.about,name='About'),
-    path('contact/',views.contact,name='Contact'),
-    path('services/',views.services,name='Services'),
-    path('team/',views.team,name='Team'),
-    path('testimonial/',views.testimonial,name='testimonial'),
-    path('appointment/',views.appointment,name='appointment'),
-
-
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include('PhysioApp.urls')),
 
 ]
